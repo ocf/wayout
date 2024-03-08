b3a7d0ac6ee5b92dd5579d10e8874280195eb88a
Oliver Ni <oliver.ni@gmail.com>
2024-03-07 18:49:44 -0800

    Add nix build

--- flake.nix
+++ flake.nix
@@ -2,19 +2,28 @@
   description = "Wayland utility to automatically terminate idle user sessions";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, cargo2nix }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
         inherit system;
+        overlays = [ cargo2nix.overlays.default ];
+      };
+
+      rustPkgs = pkgs.rustBuilder.makePackageSet {
+        rustVersion = "latest";
+        rustChannel = "stable";
+        packageFun = import ./Cargo.nix;
       };
     in
-    {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ glib pkg-config ];
+    rec {
+      packages = {
+        wayout = (rustPkgs.workspace.wayout { });
+        default = packages.wayout;
       };
     }
   );
