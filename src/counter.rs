44795bd678416093d42e030c2173327f8d9c8442
Oliver Ni <oliver.ni@gmail.com>
2024-03-07 17:33:38 -0800

    Use zbus instead of gio

--- src/counter.rs
+++ src/counter.rs
@@ -1,11 +1,34 @@
+use std::collections::HashMap;
+use std::process::{exit, Command};
 use std::sync::mpsc;
 use std::time::Duration;
 
-use gio::prelude::{ApplicationExt, ApplicationExtManual};
-use gio::{Application, Notification};
+use zbus::blocking::Connection;
+use zbus::Result;
+use zbus::{proxy, zvariant::Value};
 
+const APP_NAME: &str = "Auto Logout";
+const APP_ICON: &str = "data-warning";
 const COUNTDOWN_SECS: u64 = 20;
-const NOTIFICATION_ID: Option<&str> = Some("idle-countdown");
+
+#[proxy(
+    default_service = "org.freedesktop.Notifications",
+    default_path = "/org/freedesktop/Notifications"
+)]
+trait Notifications {
+    /// Call the org.freedesktop.Notifications.Notify D-Bus method
+    fn notify(
+        &self,
+        app_name: &str,
+        replaces_id: u32,
+        app_icon: &str,
+        summary: &str,
+        body: &str,
+        actions: &[&str],
+        hints: HashMap<&str, &Value<'_>>,
+        expire_timeout: i32,
+    ) -> Result<u32>;
+}
 
 /// A message to start the countdown
 pub struct StartCountdown {
@@ -15,41 +38,65 @@ pub struct StartCountdown {
 
 /// Run the counter application, listening for start signals from the watcher
 /// and starting the countdown when they are received. Blocks the thread.
-pub fn run(application_id: &str, start_rx: mpsc::Receiver<StartCountdown>) {
-    let application = Application::new(Some(application_id), Default::default());
-    application.connect_activate(move |application| listen(application, &start_rx));
-    application.run();
-}
+pub fn run(start_rx: mpsc::Receiver<StartCountdown>) {
+    let connection = Connection::session().unwrap();
+    let proxy = NotificationsProxyBlocking::new(&connection).unwrap();
 
-fn listen(application: &Application, start_rx: &mpsc::Receiver<StartCountdown>) {
     // Listen for start signals from the watcher and start the countdown
     while let Ok(StartCountdown { cancel_rx }) = start_rx.recv() {
-        start_countdown(application, cancel_rx);
+        if let Err(err) = start_countdown(&proxy, cancel_rx) {
+            eprintln!("Error starting countdown: {}", err);
+        };
     }
 }
 
-fn start_countdown(application: &Application, cancel_rx: mpsc::Receiver<()>) {
-    let notification = Notification::new("You still there?");
+fn start_countdown(
+    proxy: &NotificationsProxyBlocking,
+    cancel_rx: mpsc::Receiver<()>,
+) -> Result<()> {
+    let mut replaces_id = 0;
 
-    for seconds in (0..COUNTDOWN_SECS).rev() {
-        // Update the notification's body to show the remaining time
-        // Sending with the same ID will update the existing notification
-        notification.set_body(Some(&format!("Logging you out in {} seconds...", seconds)));
-        application.send_notification(NOTIFICATION_ID, &notification);
+    for seconds in (1..=COUNTDOWN_SECS).rev() {
+        // Save the ID of the notification so we can update it later
+        replaces_id = proxy.notify(
+            APP_NAME,
+            replaces_id,
+            APP_ICON,
+            "Still there?",
+            &format!("Logging you out in {} seconds...", seconds),
+            &[],
+            HashMap::new(),
+            1100,
+        )?;
 
         // Wait for 1 second, or until the cancel signal is received
         if cancel_rx.recv_timeout(Duration::from_secs(1)).is_ok() {
-            return cancel_countdown(application);
+            cancel_countdown(proxy, replaces_id)?;
+            return Ok(());
         }
     }
 
     log_out();
+    exit(0);
 }
 
-fn cancel_countdown(application: &Application) {
-    let notification = Notification::new("You're still there!");
-    notification.set_body(Some("Logging out has been canceled."));
-    application.send_notification(NOTIFICATION_ID, &notification);
+fn cancel_countdown(proxy: &NotificationsProxyBlocking, replaces_id: u32) -> Result<u32> {
+    proxy.notify(
+        APP_NAME,
+        replaces_id,
+        APP_ICON,
+        "Still there!",
+        "Logging out has been canceled.",
+        &[],
+        HashMap::new(),
+        5000,
+    )
 }
 
-fn log_out() {}
+fn log_out() {
+    Command::new("loginctl")
+        .arg("terminate-session")
+        .arg(std::env::var_os("XDG_SESSION_ID").unwrap())
+        .output()
+        .unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,14 @@ use std::thread;
 mod counter;
 mod watcher;
 
-// The main function of our program
 fn main() {
     // Create a channel for the watcher to send the start signal to the counter
     let (start_tx, start_rx) = mpsc::channel();
 
     // We run the watcher and counter in separate threads, since the counter
     // needs to be able to run its own event loop.
-    let watcher_thread: thread::JoinHandle<()> = thread::spawn(|| watcher::run(start_tx));
-    let counter_thread = thread::spawn(|| counter::run("edu.berkeley.ocf.wayout", start_rx));
+    let watcher_thread = thread::spawn(|| watcher::run(start_tx));
+    let counter_thread = thread::spawn(|| counter::run(start_rx));
 
     // Wait for both threads to finish
     watcher_thread.join().unwrap();
